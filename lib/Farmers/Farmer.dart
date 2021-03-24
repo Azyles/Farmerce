0b94b893ee9b410d5cdbc236ca9075d77f1d8779
Holland Pleskac <hollandpleskac@gmail.com>
2021-03-23 18:55:13 -0700

    spacing

--- lib/Farmers/Farmer.dart
+++ lib/Farmers/Farmer.dart
@@ -16,28 +16,37 @@ class _ExampleState extends State<Example> {
     return Scaffold(
       backgroundColor: Colors.black,
       body: FarmerContracts(),
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        selectedItemColor: Color(0xff83C264),
-        unselectedItemColor: Color(0xffDBDBDB),
-        backgroundColor: Color(0xff2B2B2B),
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: '',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: (int index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(top:10),
+        child: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          selectedItemColor: Color(0xff83C264),
+          unselectedItemColor: Color(0xffDBDBDB),
+          backgroundColor: Color(0xff2B2B2B),
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(Icons.home),
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(Icons.person),
+              ),
+              label: '',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: (int index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+        ),
       ),
     );
   }
