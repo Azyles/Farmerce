b5141c76facf1700c1de504114a79e95fdfc3080
Pranav Krishna <krishnatechpranav@gmail.com>
2021-03-26 16:40:11 -0700

    sdaF

--- lib/Consumer/ConsumerHome.dart
+++ lib/Consumer/ConsumerHome.dart
@@ -1,9 +1,11 @@
 // Home page
 import 'dart:ui';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:farmerce/Consumer/ConsumerFarm.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:math';
 
 class ConsumerHome extends StatefulWidget {
   ConsumerHome({Key key}) : super(key: key);
@@ -14,7 +16,27 @@ class ConsumerHome extends StatefulWidget {
 
 class _ConsumerHomeState extends State<ConsumerHome> {
   //Global Variables
-  List x = [1, 2, 3];
+
+  double calculateDistance(lat1, lon1, lat2, lon2) {
+    var p = 0.017453292519943295;
+    var c = cos;
+    var a = 0.5 -
+        c((lat2 - lat1) * p) / 2 +
+        c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p)) / 2;
+    return 12742 * asin(sqrt(a));
+  }
+
+  Future getLocation() {
+    Geolocator
+      .getCurrentPosition(desiredAccuracy: LocationAccuracy.best, forceAndroidLocationManager: true)
+      .then((Position position) {
+        setState(() {
+          _currentPosition = position;
+        });
+      }).catchError((e) {
+        print(e);
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,134 +84,160 @@ class _ConsumerHomeState extends State<ConsumerHome> {
                   ),
                 ),
               ),
-              StreamBuilder(
-                stream: FirebaseFirestore,
-                builder: (context, snap) {
+              StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection("Farms")
+                      .snapshots(),
+                  builder: (context, snap) {
+                    if (snap.hasData) {
+                      var data = [];
 
-                },
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                height: MediaQuery.of(context).size.height * 0.75,
-                child: ListView.builder(
-                  physics: BouncingScrollPhysics(),
-                  itemCount: x.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    FarmViewScreen("farmid")));
-                      },
-                      child: Container(
-                        margin:
-                            EdgeInsets.only(bottom: 20, left: 10, right: 10),
-                        width: MediaQuery.of(context).size.width * 0.8,
-                        height: 300,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            image: DecorationImage(
-                                fit: BoxFit.fill,
-                                image: NetworkImage(
-                                    'https://cdn.dribbble.com/users/3949861/screenshots/14387811/media/8ac916cc3132b0d6f35a77605588671d.jpg?compress=1&resize=1000x750'))),
-                        child: Stack(
-                          children: [
-                            Container(
-                              margin: EdgeInsets.only(left: 15, top: 200),
-                              child: ClipRect(
-                                child: BackdropFilter(
-                                  filter: ImageFilter.blur(
-                                    sigmaX: 10.0,
-                                    sigmaY: 10.0,
-                                  ),
-                                  child: Container(
-                                    height: 80,
-                                    width: 180,
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Container(
-                                          margin: EdgeInsets.only(
-                                              top: 10, left: 10),
-                                          child: Text("20 mi away",
-                                              style: TextStyle(
-                                                  color: Colors.grey[100])),
-                                        ),
-                                        Container(
-                                          margin:
-                                              EdgeInsets.only(top: 5, left: 10),
-                                          child: Text(
-                                            "John Doe Farms",
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 20),
+                      snap.data.docs.forEach((doc) {
+                        data.add({"id": doc.id, "value": doc.data()});
+                      });
+
+                      return Container(
+                        margin: EdgeInsets.only(top: 10),
+                        height: MediaQuery.of(context).size.height * 0.75,
+                        child: ListView.builder(
+                          physics: BouncingScrollPhysics(),
+                          itemCount: data.length,
+                          itemBuilder: (context, index) {
+                            return GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            FarmViewScreen(data[index]["id"])));
+                              },
+                              child: Container(
+                                margin: EdgeInsets.only(
+                                    bottom: 20, left: 10, right: 10),
+                                width: MediaQuery.of(context).size.width * 0.8,
+                                height: 300,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    image: DecorationImage(
+                                        fit: BoxFit.fill,
+                                        image: NetworkImage(
+                                            data[index]['value']['imageURL']))),
+                                child: Stack(
+                                  children: [
+                                    Container(
+                                      margin:
+                                          EdgeInsets.only(left: 15, top: 200),
+                                      child: ClipRect(
+                                        child: BackdropFilter(
+                                          filter: ImageFilter.blur(
+                                            sigmaX: 10.0,
+                                            sigmaY: 10.0,
+                                          ),
+                                          child: Container(
+                                            height: 80,
+                                            width: 180,
+                                            child: Column(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.start,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Container(
+                                                  margin: EdgeInsets.only(
+                                                      top: 10, left: 10),
+                                                  child: Text("20 mi away",
+                                                      style: TextStyle(
+                                                          color: Colors
+                                                              .grey[100])),
+                                                ),
+                                                Container(
+                                                  margin: EdgeInsets.only(
+                                                      top: 5, left: 10),
+                                                  child: Text(
+                                                    data[index]['value']
+                                                        ['name'],
+                                                    style: TextStyle(
+                                                        color: Colors.white,
+                                                        fontSize: 20),
+                                                  ),
+                                                ),
+                                                Container(
+                                                  margin:
+                                                      EdgeInsets.only(left: 10),
+                                                  child: Row(
+                                                    children: [
+                                                      Container(
+                                                        margin: EdgeInsets.only(
+                                                            right: 5),
+                                                        child: Icon(
+                                                          Icons.star,
+                                                          color: Colors
+                                                              .yellow[600],
+                                                        ),
+                                                      ),
+                                                      Container(
+                                                        margin: EdgeInsets.only(
+                                                            right: 5),
+                                                        child: Icon(
+                                                          Icons.star,
+                                                          color: Colors
+                                                              .yellow[600],
+                                                        ),
+                                                      ),
+                                                      Container(
+                                                        margin: EdgeInsets.only(
+                                                            right: 5),
+                                                        child: Icon(
+                                                          Icons.star,
+                                                          color: Colors
+                                                              .yellow[600],
+                                                        ),
+                                                      ),
+                                                      Container(
+                                                        margin: EdgeInsets.only(
+                                                            right: 5),
+                                                        child: Icon(
+                                                          Icons.star,
+                                                          color: Colors
+                                                              .yellow[600],
+                                                        ),
+                                                      ),
+                                                      Container(
+                                                        margin: EdgeInsets.only(
+                                                            right: 5),
+                                                        child: Icon(
+                                                          Icons.star_border,
+                                                          color: Colors
+                                                              .yellow[600],
+                                                        ),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
                                           ),
                                         ),
-                                        Container(
-                                          margin: EdgeInsets.only(left: 10),
-                                          child: Row(
-                                            children: [
-                                              Container(
-                                                margin:
-                                                    EdgeInsets.only(right: 5),
-                                                child: Icon(
-                                                  Icons.star,
-                                                  color: Colors.yellow[600],
-                                                ),
-                                              ),
-                                              Container(
-                                                margin:
-                                                    EdgeInsets.only(right: 5),
-                                                child: Icon(
-                                                  Icons.star,
-                                                  color: Colors.yellow[600],
-                                                ),
-                                              ),
-                                              Container(
-                                                margin:
-                                                    EdgeInsets.only(right: 5),
-                                                child: Icon(
-                                                  Icons.star,
-                                                  color: Colors.yellow[600],
-                                                ),
-                                              ),
-                                              Container(
-                                                margin:
-                                                    EdgeInsets.only(right: 5),
-                                                child: Icon(
-                                                  Icons.star,
-                                                  color: Colors.yellow[600],
-                                                ),
-                                              ),
-                                              Container(
-                                                margin:
-                                                    EdgeInsets.only(right: 5),
-                                                child: Icon(
-                                                  Icons.star_border,
-                                                  color: Colors.yellow[600],
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ],
+                                      ),
                                     ),
-                                  ),
+                                  ],
                                 ),
                               ),
-                            ),
-                          ],
+                            );
+                          },
                         ),
-                      ),
-                    );
-                  },
-                ),
-              ),
+                      );
+                    } else {
+                      return Container(
+                        child: Center(
+                          child: Text(
+                            "No Farms",
+                            style: TextStyle(color: Colors.white, fontSize: 25),
+                          ),
+                        ),
+                      );
+                    }
+                  }),
             ],
           )),
     );
